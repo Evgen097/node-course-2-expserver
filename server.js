b6dfa273f9d0b2f565e0459671b2e33738971e52
unknown <you@example.com>
2017-07-01 13:19:49 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var app = express();
+var hbs = require('hbs');
+var fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+
+   fs.appendFile('server.log', log +'\n');
+   console.log(log);
+   next();
+});
+
+app.use((req, res, next) => {
+    res.render('maint.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        welcome: 'Welcome on my page! Good day!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        welcome: 'Welcome on my page! Good day!',
+    });
+});
+
+
+app.get('/bad', (req, res) =>{
+   res.send({
+       errorMessege: 'Some error been heppen'
+   })
+});
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
